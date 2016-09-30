25c000ca73979d84bae6edc9a0300d6ca7279579
aleinzunza25 <inzunza251094@gmail.com>
2016-09-29 17:50:21 -0700

    Update Test.java
    Realice una modificación en esta clase

--- demo/Test.java
+++ demo/Test.java
@@ -1,13 +1,33 @@
 package demo;
 
-class Test {
-	
-	public static void main(String[] args) {
+public class Test {
+
+    public static void main(String[] args) {
+        BubbleSort BubbleSort= new BubbleSort();
+        InsertionSort InsertionSort= new InsertionSort();
+        SellSort SellSort= new SellSort();
+        Ordenacion Ord=new Ordenacion() {
+
+            @Override
+            public int[] ordenar(int[] elements) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void imprimirArreglo() {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        };
+        
+        int arreglo[] = { 20,18,19,15,17,16,11,13,14,12,10,1,5,4,3,2,6,8,7,9};
 		// Declarar un arreglo de tipo int con 20 numeros desordenados;
-		
+	
 		// Declarar un objeto de tipo Ordenacion
 		
+       
 		// Hacer la llamada al metodo de impresion
+       
+        Ord.imprimirArreglo();
 		
 	}
 
